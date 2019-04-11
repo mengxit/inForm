3614389894b2aeed3e8de0c7f16ee8813984dc1a
Mengxi Tan <mengxit@gmail.com>
2019-04-11 14:22:27 -0400

    WIC function
    WIC added;
    result variable changed;
    Age range changed;

--- js/calculator.js
+++ js/calculator.js
@@ -61,9 +61,14 @@ function handleClick() {
 		//SNAP
 		var snap_val = snap(estimated_income, household_size);
 		console.log(5 + 6);
-		$('#result').text(parseInt(snap_val));
+		$('#snap_result').text(parseInt(snap_val));
+
+		//WIC
+		var wic_val = wic(estimated_income, household_size, age_child)
+		$('#wic_result').text("WIC : " + wic_val);
+
 		return false;
-		console.log(5 + 1);
+		
     }
 }
 
@@ -108,30 +113,35 @@ function snap(estimated_income, household_size){
     return Snap_val
 }
 
+//WIC FUNCTION CALCULATION
 
-function rental_voucher(estimated_income, household_size){
+function wic(estimated_income, household_size, age_child){
 	//NEED TO UPDATE QUESTION TO INCLUDE MONTHLY INCOME NOT YEARLY & CONVERT TO INT
 	var Snap_val = 0;
-	var v1 =    ["805",	"760",	"715",	"670",	"625",	"580",	"535",	"490",	"445",	"400",	"355",	"310",	"300",	"300"];
-	var v1 =	["944",	"899",	"854",	"809",	"764",	"719",	"674",	"629",	"584",	"539",	"494",	"449",	"404",	"359"];
-	var v1 =	["1147", "1102", "1057", "1012", "967",	"922",	"877",	"832",	"787",	"742",	"697",	"652",	"607",	"562"];
-	var v1 =	["1342", "1297", "1252", "1207", "1162", "1117",	"1072",	"1027",	"982",	"937",	"892",	"847",	"802",	"757"];
-	var v1 =	["1571", "1526", "1481", "1436", "1391", "1346",	"1301",	"1256",	"1211",	"1166",	"1121",	"1076",	"1031",	"986"];
-	var v1 =	["1807", "1755", "1703", "1651", "1600", "1548",	"1496",	"1444",	"1393",	"1341",	"1289",	"1237",	"1186",	"1134"];
-	var v7 =	["2042",	"1984",	"1925",	"1867",	"1808",	"1750",	"1691",	"1633",	"1574",	"1516",	"1457",	"1399",	"1340",	"1282"];
-	var v8 =	["2278",	"2213",	"2147",	"2082",	"2017",	"1952",	"1886",	"1821",	"1756",	"1691",	"1625",	"1560",	"1495",	"1430"];
-
-
-	var Snap_Max = ["192",	"352",	"504",	"640",	"760",	"913",	"1009",	"1153",	"1297",	"1441",];
-	var IncomeArray = ["2023","2743","3463","4183","4903","5623","6343","7603","8347","9091"];
+	var IncomeArray = ["1872","2538","3204","3870","4536","5202","5868","6534","7200","7866"];
 	var HouseholdMax = 10;
-    for (var i = 1; i < HouseholdMax; i++) {
-        if(parseInt(household_size)==i) {
-        	if(parseInt(estimated_income)<=parseInt(IncomeArray[i-1])){
-        		Snap_val=Snap_Max[i-1];
+    for (var i = 0; i < HouseholdMax; i++) {
+        if(parseInt(household_size - 1) == i) {
+        	if(parseInt(estimated_income) <= parseInt(IncomeArray[i])){
+				if(age_child == "0-1")
+				{
+					Snap_val = "You qualify for both infant and mother food package."
+				}
+				if(age_child == "2-4")
+				{
+					Snap_val = "You qualify for child food package."
+				}
+				if(age_child == "Expecting in 4 Months")
+				{
+					Snap_val = "You qualify for mother food package."
+				}
         	}
 
         }
     }
-    return rental_value
+    return Snap_val
+}
+
+
+
 
